fbdab1650d4990673be1b27af6c6aa96a6024e37
WilcoApp <hello@wilco.gg>
2025-10-15 18:27:18 +0000

    Initial commit

--- basic_prompt.js
+++
@@ -1,8 +0,0 @@
-function toCamelCase(str) {
-    return str
-        .replace(/[_\-\s]+(.)?/g, (_, c) => c ? c.toUpperCase() : '')
-        .replace(/^[A-Z]/, c => c.toLowerCase());
-}
-
-// Example usage:
-console.log(toCamelCase("The Quick_Brown Fox")); // "theQuickBrownFox"
